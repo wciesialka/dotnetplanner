49a37277fded719eef1abe90a165776f9f7dc842
Willow Ciesialka <wciesialka@gmail.com>
2021-11-05 18:07:34 -0400

    Updating Calendar class.

--- planner/src/Calendar.cs
+++ planner/src/Calendar.cs
@@ -4,19 +4,19 @@ using System.Collections.Generic;
 namespace planner
 {
     class Calendar {
-        /**
-        <summary>Container class for Events</summary>
-        */
+        /// <summary>
+        /// Class that contains Events and provides methods for getting Events.
+        /// </summary>
         private List<Event> events; 
         public Calendar(){
             this.events = new List<Event>();
         }
 
         private int IndexOfEventWithID(int target){
-            /**
-            <summary>Get the lowest index of the event with the specified ID.</summary>
-            <param name="target">ID to look for</param>
-            */
+            ///<summary>Get the lowest index of the event with an ID matching <paramref name="target"/>.</summary>
+            ///<param name="target">ID to look for</param>
+            ///<returns>Lowest index the Event can be found or inserted at.</returns>
+            
             int lo = 0;
             int hi = this.events.Count;
             int mid = 0;
@@ -33,37 +33,54 @@ namespace planner
         }
 
         public void AddEvent(Event ev){
-            /**
-            <summary>This method will add the specified event to the calendar.</summary>
-            <param name="ev">Event to add.</param>
-            */
+            ///<summary>This method will add an event to the calendar. It will use the event's ID to put it in an increasing order.</summary>
+            ///<param name="ev">Event to add.</param>
+            
             int index = this.IndexOfEventWithID(ev.GetID());
             this.events.Insert(index,ev);
         }
 
-        public void RemoveEvent(int id){
-            /**
-            <summary>Remove first event with the specified ID.</summary>
-            <param name="id">ID of Event(s) to remove.</param>
-            */
+        public bool RemoveEvent(int id){
+            ///<summary>Remove first event with ID <paramref name="id"/>.</summary>
+            ///<param name="id">ID of Event(s) to remove.</param>
+            ///<returns>True if the removal is a success, False otherwise.</returns>
+            
             int i = this.IndexOfEventWithID(id);
             if(this.events[i].GetID() == id){
                 this.events.RemoveAt(i);
+                return true;
+            } else {
+                return false;
             }
         }
 
         public Event GetEvent(int id){
-            /**
-            <summary>Get the first event with the specified ID. Retyrn null if not found.</summary>
-            <param name="id">ID of Event to get.</param>
-            <returns>The first event with the specified ID. If no event found, returns null.</returns>
-            */
+        
+            ///<summary>Get the first event with the specified ID. Retyrn null if not found.</summary>
+            ///<param name="id">ID of Event to get.</param>
+            ///<returns>The first event with the specified ID. If no event found, returns null.</returns>
+            
             int i = this.IndexOfEventWithID(id);
             if(this.events[i].GetID() == id){
                 return this.events[i];
             } else {
                 return null;
             }
+        }
+
+        public List<Event> GetEventsByDate(DateTime date){
+            ///<summary>Get a list of Events that the Calendar contains that are on the same Date as <paramref name="date"/>.</summary>
+            ///<param name="date">Date to filter by.</param>
+            List<Event> list = new List<Event>();
+
+            for(int i = 0; i < this.events.Count; i++){
+                Event x = this.events[i];
+                if(x.GetDate().Date == date.Date){
+                    list.Add(x);
+                }
+            }
+
+            return list;
         }
     }
 }
