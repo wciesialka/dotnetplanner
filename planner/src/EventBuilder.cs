4996c10c5d0e2f26309d567557f8fdc1bd6b2044
Willow Ciesialka <wciesialka@gmail.com>
2021-11-05 18:45:30 -0400

    EventBuilder.BuildEvent now returns the built event.

--- planner/src/EventBuilder.cs
+++ planner/src/EventBuilder.cs
@@ -4,6 +4,9 @@ namespace planner
 {
     class EventBuilder
     {
+        /// <summary>
+        /// Builds events and adds them to the Calendar.
+        /// </summary>
         private Calendar cal;
         private int id;
         public EventBuilder(Calendar calendar){
@@ -11,9 +14,17 @@ namespace planner
             this.id = 0;
         }
 
-        public void BuildEvent(string description, DateTime date){
+        public Event BuildEvent(string description, DateTime date){
+            /// <summary>
+            /// Builds the Event and adds it to the Calendar.
+            /// </summary>
+            /// <param name="description">Description of the Event</param>
+            /// <param name="date">Date of the Event</param>
+            /// <returns>Built Event</returns>
+
             Event ev = new Event(this.id++, description, date);
             this.cal.AddEvent(ev);
+            return ev;
         }
     }
 }
